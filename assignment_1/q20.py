47e0aa59de6538afa5159bc100a4161ce9068331
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2024-12-15 21:51:07 +0530

    Add files via upload

---
+++ assignment_1/q20.py
@@ -0,0 +1,9 @@
+import math 
+a = int(input("Enter first interger: "))
+b = int(input("Enter second integer: "))
+print("The sum of a and b :" , a+b)
+print("The difference of a and b :", a-b)
+print("The quotient of when a is divided by b ", a//b)
+print("The remainder when a is divided by b", a%b)
+print("The result of log10 a", math.log10(a))
+print("The product of a and b :", a*b)
