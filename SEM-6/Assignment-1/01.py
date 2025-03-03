4d3c8c36b4656114569956c263a4fec4abadbab4
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2025-03-03 21:39:43 +0530

    Create 01.py

---
+++ SEM-6/Assignment-1/01.py
@@ -0,0 +1,6 @@
+Classes in Python are blueprints for creating objects. They encapsulate data (attributes) and behaviors (methods), promoting code reuse, modularity, and organization.
+In object-oriented programming (OOP), classes enable:
+-> Encapsulation – Bundling data and methods together.
+-> Inheritance – Allowing new classes to derive properties from existing ones.
+-> Polymorphism – Enabling different classes to be used interchangeably.
+Classes help structure large programs efficiently, making code more maintainable, scalable, and reusable. 
